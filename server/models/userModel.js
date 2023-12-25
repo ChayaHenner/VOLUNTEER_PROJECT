273ba36e786e2267a238410c8f581cdc4ee94904
TamarShwartz <tamar3205471@gmail.com>
2023-12-25 09:54:29 +0200

    add login

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -79,14 +79,13 @@ exports.validUser = (_reqBody) => {
   });
 
   return joiSchema.validate(_reqBody);
+}
+  exports.validLogin = (_reqBody) => {
+    let joiSchema = Joi.object({
+      email: Joi.string().min(2).max(99).email().required(),
+      password: Joi.string().min(3).max(99).required()
+    })
+  
+    return joiSchema.validate(_reqBody);
 };
 
-
-exports.validLogin = (_reqBody) => {
-  let joiSchema = Joi.object({
-    email: Joi.string().min(2).max(99).email().required(),
-    password: Joi.string().min(3).max(99).required()
-  })
-
-  return joiSchema.validate(_reqBody);
-}
