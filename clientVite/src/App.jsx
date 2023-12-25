53ee70ce8f4dd1892e9056114ebd3315e1b20c01
ChayaHenner <chayahenner@gmail.com>
2023-12-25 11:14:24 +0200

    header and basic comps

--- clientVite/src/App.jsx
+++ clientVite/src/App.jsx
@@ -1,34 +1,42 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './App.css'
+import HeaderAdmin from './comps_admin/headerAdmin'
+import HeaderUser from './comps_users/headerUser'
+import Login from './comps_users/login'
+import SignUp from './comps_users/signUp'
+import MyMissions from './comps_users/myMissions'
+import PostMission from './comps_users/postMission'
+import Home from './comps_main/home'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/admin/*" element={<HeaderAdmin />} />
+        <Route path="/*" element={<HeaderUser />} />
+      </Routes>
+
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/*" element={<h2>Page 404</h2>}/>
+
+        
+        <Route path="/login" element={<Login />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/my-missions" element={<MyMissions />} />
+        <Route path="/post-mission" element={<PostMission />} />
+        {/* {adminRoutes()} */}
+
+     
+      </Routes>
+
+
+
+    </BrowserRouter>
   )
 }
 
