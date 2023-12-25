13a5b2fa9d23dcb985a5427db7c71952ab8e3858
BatshevaYakovson <122361591+BatshevaYakovson@users.noreply.github.com>
2023-12-25 09:54:48 +0200

    try to work on chaya mongo

--- server/db/mongoconnect.js
+++ server/db/mongoconnect.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
-const {config} = require("../config/secret")
+const { config } = require("../config/secret")
 
 main().catch(err => console.log(err));
 
 async function main() {
-    mongoose.set('strictQuery' , false);
+  mongoose.set('strictQuery', false);
   await mongoose.connect('mongodb://127.0.0.1:27017/project');
   // await mongoose.connect(`mongodb+srv://${config.userDb}:${config.passDb}@clusterchaya.zoha4yp.mongodb.net/PROJECT`);
 
