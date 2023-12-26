5a5040f2ec5ce1bdfe845cdad73efa356a966152
ChayaHenner <chayahenner@gmail.com>
2023-12-26 11:47:05 +0200

    profile

--- server/db/mongoconnect.js
+++ server/db/mongoconnect.js
@@ -8,8 +8,8 @@ main().catch(err => console.log(err));
 async function main() {
   mongoose.set('strictQuery', false);
 
-  // await mongoose.connect('mongodb://127.0.0.1:27017/project');
-  await mongoose.connect(`mongodb+srv://${config.userDb}:${config.passDb}@clusterchaya.zoha4yp.mongodb.net/VOLUNTEER_PROJECT`);
+  await mongoose.connect('mongodb://127.0.0.1:27017/VOLUNTEER_PROJECT');
+  // await mongoose.connect(`mongodb+srv://${config.userDb}:${config.passDb}@clusterchaya.zoha4yp.mongodb.net/VOLUNTEER_PROJECT`);
   // await mongoose.connect(`mongodb+srv://chayahenner:R2023@clusterchaya.zoha4yp.mongodb.net/project`);
 
   console.log("mongo connect")
