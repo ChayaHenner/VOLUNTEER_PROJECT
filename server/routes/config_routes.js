aa363cf594e551e2cf85e2b4c9149183325af63c
BatshevaYakovson <122361591+BatshevaYakovson@users.noreply.github.com>
2023-12-26 15:06:23 +0200

    mission route almost finish and reviews route working

--- server/routes/config_routes.js
+++ server/routes/config_routes.js
@@ -1,9 +1,11 @@
 const indexR = require("./index");
 const usersR = require("./users");
 const missionsR = require("./missions")
+const reviewsR = require("./reviews")
 
 exports.routesInit = (app) => {
-  app.use("/",indexR);
-  app.use("/users",usersR);
-  app.use("/missions",missionsR);
+  app.use("/", indexR);
+  app.use("/users", usersR);
+  app.use("/missions", missionsR);
+  app.use("/reviews", reviewsR);
 }
