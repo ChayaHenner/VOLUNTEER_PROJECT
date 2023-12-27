7d6d32a9dd63f4c6944bf237aa41ed0ff60af205
TamarShwartz <tamar3205471@gmail.com>
2023-12-27 11:42:01 +0200

    user

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -13,7 +13,9 @@ let userSchema = new mongoose.Schema({
   address: String,
   birth_date: Date,
   img_url: String,
-  rating: Number,
+  rating: {
+    type: Number, default: 0
+  },
   date_created: {
     type: Date, default: Date.now()
   },
