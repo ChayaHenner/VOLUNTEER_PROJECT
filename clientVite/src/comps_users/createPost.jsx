5e90ceeb0c31898761c74a487822f3a22903a5e8
ChayaHenner <chayahenner@gmail.com>
2023-12-27 14:35:01 +0200

    post

--- clientVite/src/comps_users/createPost.jsx
+++ clientVite/src/comps_users/createPost.jsx
@@ -1,20 +1,25 @@
-import React from 'react';
+import React ,{useState} from 'react';
 import { useForm } from 'react-hook-form';
 import { SERVER_URL, apiRequest } from '../serverConnect/api';
 import Cookies from 'js-cookie';
+import { uploadImageToStorage } from '../helper/helper';
 
 const CreatePost = () => {
   const { register, handleSubmit, formState: { errors }, getValues } = useForm();
+  const [selectedImage, setSelectedImage] = useState(null);
 
   const onSubPost = async(data) => {
+    const imageUrl = await uploadImageToStorage(selectedImage);
+    data.img_url = imageUrl;
+
     console.log(data);
-    // let url = SERVER_URL + "/posts/";
-    // try {
-    //   let resp = await apiRequest(url, "POST", data);
-    //   console.log("mission added");
-    // } catch (err) {
-    //   console.log("ERROR ", err);
-    // }
+    let url = SERVER_URL + "/posts/";
+    try {
+      let resp = await apiRequest(url, "POST", data);
+      console.log("post added");
+    } catch (err) {
+      console.log("ERROR ", err);
+    }
   };
 
   return (
@@ -32,10 +37,12 @@ const CreatePost = () => {
           {errors.description && <div className="text-red-500 text-xs">Description is required</div>}
         </div>
         <div className="mb-4">
-          <label className="block text-sm font-medium text-gray-700">Image Input:</label>
-          <input {...register('image', { required: true })} type="file" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500" />
-          {errors.image && <div className="text-red-500 text-xs">Image is required</div>}
-        </div>
+            <label className="block text-sm font-medium text-gray-700">
+              Profile Image:</label>
+            <input {...register('img_url')} type="file" accept="image/*" onChange={(e) => setSelectedImage(e.target.files[0])} className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"/>
+            {/* <input {...register('img_url')} type="file" accept="image/*" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500" /> */}
+          </div>
+
         <button type="submit" className="bg-purple-500 text-white px-4 py-2 rounded-md mt-4">Create Post</button>
       </form>
     </div>
--- server/validation/postValidation.js
+++ server/validation/postValidation.js
@@ -2,7 +2,7 @@ const Joi = require("joi");
 
 exports.validPost = (_reqBody) => {
     let joiSchema = Joi.object({
-        img_url: Joi.string().min(2).max(99).allow(null, ""),
+        img_url: Joi.string().allow(null, ""),
         title: Joi.string().min(2).max(99).required(),
         description: Joi.string().min(2).max(99).required(),
         like_nums: Joi.number(),
