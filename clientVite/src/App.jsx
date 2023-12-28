b49dbacf85580dee296fea9daeba5b2129ca4dcd
BatshevaYakovson <122361591+BatshevaYakovson@users.noreply.github.com>
2023-12-28 12:16:53 +0200

    changes

--- clientVite/src/App.jsx
+++ clientVite/src/App.jsx
@@ -18,7 +18,6 @@ import ForgotPassword from './comps_users/forgotPass'
 import ResetPassword from './comps_users/resetPass'
 import ResetPasswordPage from './comps_users/ResetPasswordPage'
 // import AddressInput from './comps_users/addressInput'
-// import AddressInput from './comps_users/addressInput'
 import ViewMissions from './comps_users/viewMissions'
 import Logout from './comps_users/logOut'
 import ViewUser from './comps_users/viewUser'
@@ -46,18 +45,18 @@ function App() {
           <Route path="/my-profile" element={<ProfilePage />} />
           <Route path="/edit-profile" element={<EditProfile />} />
           <Route path="/view-missions" element={<ViewMissions />} />
-          <Route path="/ab" element={<AddressInput />} />
+          {/* <Route path="/ab" element={<AddressInput />} /> */}
           <Route path="/view-missions" element={<ViewMissions />} />
           <Route path="/logout" element={<Logout />} />
-          <Route path="/ab" element={<AddressInput />} />
+          {/* <Route path="/ab" element={<AddressInput />} /> */}
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/reset-password/:token" element={<ResetPassword />} />
           <Route path="/resetPass/:token" element={<ResetPasswordPage />} />
           <Route path="/view-user/:id" element={<ViewUser />} />
-          {/* <Route path="/ab" element={<AddressInput/>} /> */}
-          {/* <Route path="/ab" element={<AddressInput />} /> */}
           <Route path="/view-missions" element={<ViewMissions />} />
           <Route path="/logout" element={<Logout />} />
+          {/* <Route path="/view-user/:userId" element={<ProfilePage />} /> */}
+
           {/* <Route path="/ab" element={<AddressInput/>} /> */}
           {/* {adminRoutes()} */}
 
--- clientVite/src/comps_users/viewMissions.jsx
+++ clientVite/src/comps_users/viewMissions.jsx
@@ -1,43 +1,6 @@
-// import React, { useEffect, useState } from 'react';
-// import { apiRequestGet, SERVER_URL } from '../serverConnect/api';
-
-// const ViewMissions = () => {
-//   const [missions, setMissions] = useState([]);
-
-//   useEffect(() => {
-//     const fetchMissions = async () => {
-//       try {
-//         const url = `${SERVER_URL}/missions`; // Update the URL according to your API
-//         const response = await apiRequestGet(url);
-//         setMissions(response.data);
-//       } catch (error) {
-//         console.error('Error fetching missions:', error);
-//       }
-//     };
-
-//     fetchMissions();
-//   }, []); // Empty dependency array ensures the effect runs only once on mount
-
-//   return (
-//     <div>
-//       <h2>Missions</h2>
-//       {missions.map((mission) => (
-//         <div key={mission._id}>
-//           <h3>{mission.title}</h3>
-//           <p>{mission.description}</p>
-//           {/* You can display additional mission details and nomination proposal here */}
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default ViewMissions;
-
-
-
 import React, { useEffect, useState } from 'react';
 import { apiRequestGet, SERVER_URL } from '../serverConnect/api';
+import { Link } from 'react-router-dom';
 
 const ViewMissions = () => {
   const [missions, setMissions] = useState([]);
@@ -69,7 +32,9 @@ const ViewMissions = () => {
           <h3 className="text-xl font-semibold mb-2">{mission.title}</h3>
           <p className="text-gray-600 mb-2">{mission.description}</p>
           <div className="flex justify-between items-center">
-            <p className="text-sm text-gray-500">{`Created by: ${mission.user_creator}`}</p>
+            <Link to={`/view-user/${mission.user_creator}`}>
+              <p className="text-sm text-gray-500">{`Created by: ${mission.user_creator}`}</p>
+            </Link>
             <button
               onClick={() => handleTaskTake(mission._id)}
               className="bg-blue-500 text-white px-4 py-2 rounded-md"
