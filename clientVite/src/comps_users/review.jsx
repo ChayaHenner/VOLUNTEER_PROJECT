e7bb95d73550ee32446b5bab25b482a8fbd0a835
ChayaHenner <chayahenner@gmail.com>
2023-12-28 11:43:07 +0200

    review

--- clientVite/src/comps_users/review.jsx
+++ clientVite/src/comps_users/review.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
-
-const Review = ({review}) => {
+import StarIcon from './starIcon'
+const Review = ({ review }) => {
   // const review = props.review; // Destructuring props
-
   return (
     <div className="rounded-md p-4 m-1 shadow-md">
-      <h2 className="text-xl font-semibold mb-2">{review.title}</h2>
-      <p className="text-gray-600 mb-2">{review.description}</p>
-      <div className="flex items-center">
-        <span className="text-yellow-500">{review.rating}</span>
-      </div>
-      <p className="text-sm text-gray-500 mt-2">Review by {review.user_creator}</p>
+      <article>
+        <div class="flex items-center mb-4">
+          <img class="w-10 h-10 me-4 rounded-full" src="/docs/images/people/profile-picture-5.jpg" alt="" />
+          <div class="font-medium dark:text-white">
+            <p>user_creator{review.user_creator} <time datetime="2014-08-16 19:00" class="block text-sm text-gray-500 dark:text-gray-400"></time></p>
+          </div>
+        </div>
+        <div class="flex  mb-1 space-x-1 rtl:space-x-reverse">
+          <h3 class="ms-2 text-sm  text-gray-300 dark:text-white">{review.rating}</h3>
+          <StarIcon rating={review.rating > 0 ? review.rating : 0} />
+          <h3 class="ms-2 text-sm font-semibold text-gray-900 dark:text-white">{review.title}</h3>
+        </div>
+        <p class="mb-2 text-gray-500 dark:text-gray-400">{review.description}</p>
+        <aside>
+        </aside>
+      </article>
+
     </div>
   );
 };
