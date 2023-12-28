b368a3c36d65163234871208c58f9043fc516bfc
BatshevaYakovson <122361591+BatshevaYakovson@users.noreply.github.com>
2023-12-28 15:11:22 +0200

    add filter

---
+++ clientVite/src/comps_users/dateFilter.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { apiRequestGet } from 'path-to-your-api-request-file'; // Import your API request function
+
+const DateFilter = ({ updateMissions }) => {
+    const [startDate, setStartDate] = useState('');
+    const [endDate, setEndDate] = useState('');
+    const [startTime, setStartTime] = useState('');
+    const [endTime, setEndTime] = useState('');
+
+    const handleFilter = async () => {
+        try {
+            // Perform API request to filter missions
+            const response = await apiRequestGet(`/missions/byDateTime?startDate=${startDate}&endDate=${endDate}&startTime=${startTime}&endTime=${endTime}`);
+            const filteredMissions = response.data; // Assuming your API response contains filtered missions
+
+            // Update state or perform any other action with the filtered missions
+            updateMissions(filteredMissions);
+        } catch (error) {
+            console.error('Error filtering missions:', error);
+        }
+    };
+
+    return (
+        <div>
+            <label>Start Date:</label>
+            <input type="date" value={startDate} onChange={(e) => setStartDate(e.target.value)} />
+
+            <label>End Date:</label>
+            <input type="date" value={endDate} onChange={(e) => setEndDate(e.target.value)} />
+
+            <label>Start Time:</label>
+            <input type="time" value={startTime} onChange={(e) => setStartTime(e.target.value)} />
+
+            <label>End Time:</label>
+            <input type="time" value={endTime} onChange={(e) => setEndTime(e.target.value)} />
+
+            <button onClick={handleFilter}>Filter</button>
+        </div>
+    );
+};
+
+export default DateFilter;
--- clientVite/src/comps_users/viewMissions.jsx
+++ clientVite/src/comps_users/viewMissions.jsx
@@ -1,11 +1,86 @@
+// import React, { useEffect, useState } from 'react';
+// import { apiRequestGet, apiRequest, SERVER_URL, apiRequestNoBody } from '../serverConnect/api';
+// import { Link } from 'react-router-dom';
+
+// const ViewMissions = () => {
+//   const [missions, setMissions] = useState([]);
+
+//   useEffect(() => {
+//     const fetchMissions = async () => {
+//       try {
+//         const url = `${SERVER_URL}/missions`;
+//         const response = await apiRequestGet(url);
+//         setMissions(response.data);
+//       } catch (error) {
+//         console.error('Error fetching missions:', error);
+//       }
+//     };
+
+//     fetchMissions();
+//   }, []);
+
+//   const handleTakeTask = async (missionId) => {
+//     try {
+//       console.log('dsfsdafs')
+//       // Send a request to the backend to add the user to the mission's
+//       // http://localhost:3001/missions/addInterested/65895cdc1e88238084d3661e interested array
+//       const response = await apiRequestNoBody(`${SERVER_URL}/missions/addInterested/${missionId}`, 'PUT');
+
+//       if (response.data.success) {
+//         // Handle success, e.g., show a message or update the UI
+//         alert('User added to interested list');
+//       }
+//     } catch (error) {
+//       // Handle error, e.g., show an error message
+//       console.error('Error taking task:', error);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <h2>Missions</h2>
+//       {missions.map((mission) => {
+//         const userArray = mission.user_creator.split(',');
+//         const id = userArray[0];
+//         const name = userArray[1];
+
+//         return (
+//           <div key={mission._id} className="border p-4 mb-4">
+//             <h3 className="text-xl font-semibold mb-2">{mission.title}</h3>
+//             <p className="text-gray-600 mb-2">{mission.description}</p>
+//             <div className="flex justify-between items-center">
+//               <Link to={`/view-user/${id}`}>
+//                 <p className="text-sm text-gray-500">{`Created by: ${name}`}</p>
+//               </Link>
+//               <button
+//                 onClick={() => handleTakeTask(mission._id)}
+//                 className="bg-blue-500 text-white px-4 py-2 rounded-md"
+//               >
+//                 Take Task
+//               </button>
+//             </div>
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// };
+
+// export default ViewMissions;
+
+
 import React, { useEffect, useState } from 'react';
 import { apiRequestGet, apiRequest, SERVER_URL, apiRequestNoBody } from '../serverConnect/api';
 import { Link } from 'react-router-dom';
+
+// Import the new DateFilter component
+import DateFilter from './dateFilter'; // Update the path based on your project structure
 
 const ViewMissions = () => {
   const [missions, setMissions] = useState([]);
 
   useEffect(() => {
+    // Fetch missions on component mount
     const fetchMissions = async () => {
       try {
         const url = `${SERVER_URL}/missions`;
@@ -21,9 +96,7 @@ const ViewMissions = () => {
 
   const handleTakeTask = async (missionId) => {
     try {
-      console.log('dsfsdafs')
-      // Send a request to the backend to add the user to the mission's
-      // http://localhost:3001/missions/addInterested/65895cdc1e88238084d3661e interested array
+      // Send a request to the backend to add the user to the mission's interested array
       const response = await apiRequestNoBody(`${SERVER_URL}/missions/addInterested/${missionId}`, 'PUT');
 
       if (response.data.success) {
@@ -36,9 +109,18 @@ const ViewMissions = () => {
     }
   };
 
+  // Update missions based on filtered data
+  const updateMissions = (filteredMissions) => {
+    setMissions(filteredMissions);
+  };
+
   return (
     <div>
       <h2>Missions</h2>
+
+      {/* Add the DateFilter component to handle date and time range filtering */}
+      <DateFilter updateMissions={updateMissions} />
+
       {missions.map((mission) => {
         const userArray = mission.user_creator.split(',');
         const id = userArray[0];
@@ -67,3 +149,4 @@ const ViewMissions = () => {
 };
 
 export default ViewMissions;
+
