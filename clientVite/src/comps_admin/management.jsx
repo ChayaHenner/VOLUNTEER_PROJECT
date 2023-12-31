945abd5ae29eeefbca0c09168e70f2440cd6045d
TamarShwartz <tamar3205471@gmail.com>
2023-12-31 12:21:59 +0200

    admin

---
+++ clientVite/src/comps_admin/management.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { SERVER_URL, apiRequestGet, apiRequestNoBody } from '../serverConnect/api';
+
+const Management = () => {
+    const [userList, setUserList] = useState([]);
+
+    useEffect(() => {
+        // Fetch missions on component mount
+        const fetchUserList = async () => {
+            try {
+                const url = `${SERVER_URL}/report`;
+                const response = await apiRequestGet(url);
+                setUserList(response.data);
+            } catch (error) {
+                console.error('Error fetching userList:', error);
+            }
+        };
+        fetchUserList();
+    }, []);
+
+    const handleDelete = async (id) => {
+        try {
+            const url = `${SERVER_URL}/users/block/${id}`;
+            await apiRequestNoBody(url, "PUT");
+            fetchUserList();
+        } catch (error) {
+            console.error('Error deleting report:', error);
+        }
+    };
+
+    return (
+        <div className="container mx-auto mt-8">
+            <table className="min-w-full border border-gray-300">
+                <thead>
+                    <tr>
+                        <th className="border p-2">Reporter</th>
+                        <th className="border p-2">Reportee</th>
+                        <th className="border p-2">Message</th>
+                        <th className="border p-2">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {userList.map((report) => (
+                        <tr key={report._id}>
+                            <td className="border p-2">{report.id_reporter}</td>
+                            <td className="border p-2">{report.id_reportee}</td>
+                            <td className="border p-2">{report.Message}</td>
+                            <td className="border p-2">
+                                <button
+                                    className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                                    onClick={() => handleDelete(report.id_reportee)}
+                                >
+                                    Block
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default Management;
--- clientVite/src/comps_admin/viewUsers.jsx
+++ clientVite/src/comps_admin/viewUsers.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom'
+import { SERVER_URL, apiRequestGet } from '../serverConnect/api';
 
 const ViewUsers = () => {
+  const [userList, setUserList] = useState([]);
+
+  useEffect(() => {
+    // Fetch missions on component mount
+    const fetchUserList = async () => {
+      try {
+        const url = `${SERVER_URL}/users/usersList`;
+        const response = await apiRequestGet(url);
+        setUserList(response.data);
+      } catch (error) {
+        console.error('Error fetching userList:', error);
+      }
+    };
+    fetchUserList();
+  }, []);
+
   return (
-    <div>view users and will change to admin and block users</div>
-  )
+    <div className="container mx-auto mt-10">
+      <h2 className="text-3xl font-bold mb-8">User List</h2>
+
+      <table className="min-w-full border border-gray-300">
+        <thead>
+          <tr>
+            <th className="py-2 px-4 border-b">Full Name</th>
+            <th className="py-2 px-4 border-b">TZ</th>
+            <th className="py-2 px-4 border-b">Role</th>
+            <th className="py-2 px-4 border-b">Profile Page</th>
+          </tr>
+        </thead>
+        <tbody>
+          {userList.map(user => (
+            <tr key={user._id}>
+              <td className="py-2 px-4 border-b">{user.full_name}</td>
+              <td className="py-2 px-4 border-b">{user.tz}</td>
+              <td className="py-2 px-4 border-b">{user.role}</td>
+              <td className="py-2 px-4 border-b">
+                <Link to={`/view-user/${user._id}`} className="text-blue-500">Profile Page</Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
-export default ViewUsers
+export default ViewUsers;
