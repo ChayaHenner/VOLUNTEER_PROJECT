090ad537047c124dca05c5b5e77830f2ca1edcb7
ChayaHenner <chayahenner@gmail.com>
2023-12-31 10:07:27 +0200

    Update viewMissions.jsx

--- clientVite/src/comps_users/viewMissions.jsx
+++ clientVite/src/comps_users/viewMissions.jsx
@@ -128,7 +128,7 @@ const ViewMissions = () => {
 
         return (
           <>
-            <div key={mission._id} className="border p-4 mb-4">
+            {/* <div key={mission._id} className="border p-4 mb-4">
               <h3 className="text-xl font-semibold mb-2">{mission.title}</h3>
               <p className="text-gray-600 mb-2">{mission.description}</p>
               <div className="flex justify-between items-center">
@@ -142,31 +142,23 @@ const ViewMissions = () => {
                   Take Task
                 </button>
               </div>
-            </div>
+            </div> */}
 
-            <div key={mission._id} class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-              {/* <a href="#"> */}
+            <div key={mission._id} class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-4">
               <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{mission.title}</h5>
-              {/* </a>
               <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{mission.description}</p>
-              <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                Take Task
+              <div className="flex">
+                <Link className='w-1/2' to={`/view-user/${id}`}>
+                  <p className="text-sm text-gray-500">{`Created by: ${name}`}</p>
+                </Link>
 
-                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                </svg>
-              </a> */}
-              <Link to={`/view-user/${id}`}>
-                <p className="text-sm text-gray-500">{`Created by: ${name}`}</p>
-              </Link>
-
-              <button
-                onClick={() => handleTakeTask(mission._id)}
-                className="bg-blue-500 text-white px-4 py-2 rounded-md"
-              >
-                Take Task
-              </button>
-
+                <button 
+                  onClick={() => handleTakeTask(mission._id)}
+                  className="w-1/2 bg-blue-500 text-white px-4 py-2 rounded-md m-2"
+                >
+                  Take Task
+                </button>
+              </div>
             </div>
           </>
         );
